e329bd1c04b000759e1ca9e5772defadebe607f0
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-01 20:03:01 -0800

    added file

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom"
 
 
 function ErrorPage() {
     return (
-        <div>
-
+        <div className=" m-20 ">
+            <div>
+                <div className=" flex justify-center ">
+                    <img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif" alt="" />
+                </div>
+                <NavLink to='/' style={({ isActive }) => ({ color: isActive ? "yellow" : "" })}>
+                    <div><button className=" bg-black btn w-[300px] text-white ">Go Back To Home Page</button></div>
+                </NavLink>
+            </div>
         </div>
     )
 }
