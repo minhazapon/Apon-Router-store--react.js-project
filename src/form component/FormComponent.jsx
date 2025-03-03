535d66c2dfc4fade14e90e0ef8c7379fdf4cb1ad
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-03 01:47:04 -0800

    added file

--- src/form component/FormComponent.jsx
+++ src/form component/FormComponent.jsx
@@ -15,10 +15,19 @@ function FormCompo() {
 
                 <Form>
 
-                    <div>
+                    <div className=" mt-10 ">
 
                         <div>
-                            <input type="text" name="name" />
+                            <input placeholder="name" className=" p-1 h-[50px] w-full border-[1px] border-cyan-600 " type="text" name="name" />
+                        </div>
+                        <div>
+                            <input placeholder="Email" className=" mt-5 p-1 h-[50px] w-full border-[1px] border-cyan-600 " type="email" name="email" />
+                        </div>
+                        <div>
+                            <input placeholder="message" className=" mt-5 p-1 h-[50px] w-full border-[1px] border-cyan-600 " type="text" name="message" />
+                        </div>
+                        <div>
+                            <input className=" btn w-full mt-5 bg-black text-white " type="submit" value="Submit" />
                         </div>
 
 
