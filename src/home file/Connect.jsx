6df01e9d43354c1da509bfbcfd7d3722fc95f253
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 03:35:48 -0800

    added file

--- src/home file/Connect.jsx
+++ src/home file/Connect.jsx
@@ -7,10 +7,25 @@ function Connect() {
     return (
         <div className=" ml-10 mr-10 p-5 mt-20 mb-10 ">
             <div>
-
-
-
-
+                <div>
+                    <p className="text-center text-4xl text-black">Connect to everything important to you</p>
+                </div>
+                <div className=" flex justify-center items-center gap-10 mt-10 ">
+                    <div className=" border-[1px] w-fit p-5 rounded-xl ">
+                        <div className=" flex justify-center">
+                            <img className=" h-[500px] rounded-xl " src="https://img.freepik.com/free-photo/wi-fi-router-with-blue-optical-fiber_23-2148779307.jpg?t=st=1741087750~exp=1741091350~hmac=a17ad4714d75042fc741750510fc0c4339e0c336370bcaff0b5061bae474be9f&w=740" alt="" />
+                        </div>
+                        <p className=" text-xl text-center mt-5 ">An untethered watch for an<br></br> untethered life.</p>
+                        <p className=" text-center mt-5 font-thin ">The new Galaxy router connects via wifi and LTE,<br></br> so you can text, call, and<br></br> run your day while leaving your phone at home.</p>
+                    </div>
+                    <div className=" border-[1px] w-fit p-5 rounded-xl ">
+                        <div className=" flex justify-center">
+                            <img className=" h-[500px] rounded-xl " src="https://img.freepik.com/free-photo/top-view-wi-fi-router-with-smart-devices-house-figurine_23-2148779303.jpg?t=st=1741087830~exp=1741091430~hmac=b9ff9c0d8edb248bb43d8fe9f0219b8a2a48b8215d66492dbcc073cb4e02d606&w=740" alt="" />
+                        </div>
+                        <p className=" text-xl text-center mt-5 ">An untethered watch for an<br></br> untethered life.</p>
+                        <p className=" text-center mt-5 font-thin ">The new Galaxy router connects via wifi and LTE,<br></br> so you can text, call, and<br></br> run your day while leaving your phone at home.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
