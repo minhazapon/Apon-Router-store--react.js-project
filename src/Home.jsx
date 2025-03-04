2495a7291846778e8b61091bbc7a312e57149227
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 01:52:54 -0800

    added file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
+import Explore from "./home file/Explore"
 import HomeBanner from "./home file/HomeBanner"
 
 function Home() {
     return (
         <div>
             <HomeBanner></HomeBanner>
+            <Explore></Explore>
 
         </div>
     )
--- src/home file/Explore.jsx
+++ src/home file/Explore.jsx
@@ -2,7 +2,15 @@
 
 function Explore() {
     return (
-        <div>
+        <div className=" mt-20 ml-10 mr-10 p-5 ">
+
+            <div>
+                <div>
+                    <p>Explore other best Router devices.</p>
+                </div>
+
+
+            </div>
 
         </div>
     )
