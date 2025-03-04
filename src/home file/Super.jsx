298e89b92c59860d639699e4feca3746e19e3c4d
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 02:10:50 -0800

    added file

--- src/home file/Super.jsx
+++ src/home file/Super.jsx
@@ -3,8 +3,7 @@
 function Super() {
     return (
         <div>
-            <div className=" mt-20 ml-10 mr-10 p-5 ">
-
+            <div className=" mt-20 ml-10 mr-10 p-5 flex justify-between items-center ">
                 <div>
                     <p className=" text-5xl font-bold ">The new super<br></br> powerful Galaxy<br></br> Note9</p>
                     <p className=" mt-5 text-xl ">Meet the latest member of the router family.</p>
@@ -12,11 +11,9 @@ function Super() {
                         <button className="btn bg-black text-white w-[300px] mt-8">Buy Now</button>
                     </div>
                 </div>
-
                 <div>
-                    <img src="https://cdn-icons-png.flaticon.com/128/1864/1864077.png" alt="" />
+                    <img className=" h-[300px] rounded-xl " src="https://img.freepik.com/free-photo/high-angle-wi-fi-router-with-vacuum-cleaner_23-2148779238.jpg?t=st=1741082917~exp=1741086517~hmac=81d6fb2c739f2f3468a3c6464260437fcc9f417ebb164a0016f4673cf70c8614&w=1060" alt="" />
                 </div>
-
             </div>
         </div>
     )
