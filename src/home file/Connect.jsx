4b5092ccbee4313504555bd42cb8955ab0be29d6
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 03:37:54 -0800

    added file

--- src/home file/Connect.jsx
+++ src/home file/Connect.jsx
@@ -1,8 +1,5 @@
 
-
-
 function Connect() {
-
 
     return (
         <div className=" ml-10 mr-10 p-5 mt-20 mb-10 ">
@@ -22,7 +19,8 @@ function Connect() {
                         <div className=" flex justify-center">
                             <img className=" h-[500px] rounded-xl " src="https://img.freepik.com/free-photo/top-view-wi-fi-router-with-smart-devices-house-figurine_23-2148779303.jpg?t=st=1741087830~exp=1741091430~hmac=b9ff9c0d8edb248bb43d8fe9f0219b8a2a48b8215d66492dbcc073cb4e02d606&w=740" alt="" />
                         </div>
-                        <p className=" text-xl text-center mt-5 ">An untethered watch for an<br></br> untethered life.</p>
+                        <p className=" text-xl text-center mt-5 ">Turn your Network<br></br>
+                            into your router.</p>
                         <p className=" text-center mt-5 font-thin ">The new Galaxy router connects via wifi and LTE,<br></br> so you can text, call, and<br></br> run your day while leaving your phone at home.</p>
                     </div>
                 </div>
