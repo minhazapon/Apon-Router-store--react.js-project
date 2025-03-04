a5074240e7abf28b15ef405b0979ebe8132d1f78
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 02:04:45 -0800

    added file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import Explore from "./home file/Explore"
 import HomeBanner from "./home file/HomeBanner"
+import Super from "./home file/Super"
 
 function Home() {
     return (
         <div>
             <HomeBanner></HomeBanner>
             <Explore></Explore>
+            <Super></Super>
 
         </div>
     )
--- src/home file/Super.jsx
+++ src/home file/Super.jsx
@@ -3,7 +3,17 @@
 function Super() {
     return (
         <div>
-            <div></div>
+            <div className=" mt-20 ml-10 mr-10 p-5 ">
+
+                <div>
+                    <p className=" text-5xl font-bold ">The new super<br></br> powerful Galaxy<br></br> Note9</p>
+                    <p className=" mt-5 text-xl ">Meet the latest member of the router family.</p>
+                    <div>
+                        <button className="btn bg-black text-white w-[300px] mt-8">Buy Now</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
