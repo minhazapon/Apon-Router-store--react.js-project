96de56198a1f26b0910d489268254936a46706ce
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 01:58:27 -0800

    added file

--- src/home file/Explore.jsx
+++ src/home file/Explore.jsx
@@ -8,8 +8,14 @@ function Explore() {
                 <div>
                     <p className=" text-center text-3xl">Explore other best <span className=" text-4xl text-[#72afa6]">ROUTER</span> devices.</p>
                 </div>
-
-
+                <div>
+                    <div className=" grid  md:grid-cols-4 gap-5 mt-10 ">
+                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                    </div>
+                </div>
             </div>
 
         </div>
