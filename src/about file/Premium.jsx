131fc007921cfef8883d05c0ec3fc47a3da5c932
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-05 01:00:46 -0800

    added file

--- src/about file/Premium.jsx
+++ src/about file/Premium.jsx
@@ -1,12 +1,57 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
 
 
 function Premium() {
 
+    const [wifi, setWifi] = useState([])
+
+    useEffect(() => {
+
+        axios.get('wifi.json')
+            .then(res => {
+                setWifi(res.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    })
 
     return (
-        <div>
-
-        </div>
+        <>
+            <div>
+                <div className=" ml-10 mr-10 p-5 mt-20 ">
+                    <div>
+                        <p className=" text-center text-5xl font-semibold text-black ">Some Premium Products</p>
+                    </div>
+                    <div className=" flex justify-center mt-10 ">
+                        <div className=" grid  md:grid-cols-3 gap-5 ">
+                            {
+                                wifi.map(products => <ul key={products.id}>
+                                    <div className="card bg-base-100 w-96 h-[500px] border-[1px] ">
+                                        <figure>
+                                            <img
+                                                className=" w-full h-[350px] "
+                                                src={products.image}
+                                                alt="Shoes" />
+                                        </figure>
+                                        <div className="card-body">
+                                            <h2 className="card-title"> {products.name} </h2>
+                                            <p> {products.details} </p>
+                                            <div className="card-actions justify-end">
+                                                <button className="btn bg-black text-white w-full mt-5 ">Buy Now</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ul>)
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
