27bd8bced7b58d89c33651ae61b205fb22f77972
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 21:33:06 -0800

    added file

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,21 @@
 
 function Footer() {
     return (
-        <div>
+        <>
+            <div>
+                <div className=" mt-20 ml-10 mr-10 p-5">
 
-        </div>
+                    <div>
+
+
+
+
+
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
