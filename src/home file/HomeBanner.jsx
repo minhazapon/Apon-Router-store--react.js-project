9914ddc38a412315b6b3d11a17b2fa24c0f38ea6
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 20:55:30 -0800

    added file

--- src/home file/HomeBanner.jsx
+++ src/home file/HomeBanner.jsx
@@ -3,18 +3,18 @@
 function HomeBanner() {
     return (
         <div>
-            <div className=" ml-10 mr-10 p-5 flex justify-between items-center">
+            <div className=" ml-10 mr-10 p-5 flex-col md:flex-row lg:flex-row flex justify-between items-center">
                 <div>
-                    <p className=" text-6xl font-bold ">Discover a <br></br>world without<br></br> limits
+                    <p className=" text-center  md:text-start text-xl md:text-6xl font-bold ">Discover a <br></br>world without<br></br> limits
                     </p>
-                    <p className="mt-5 font-thin">It doesn't take a genius to see why<br></br> switching to the Galaxy S9 | S9+ is a good idea.
+                    <p className="  text-center  md:text-start mt-5 font-thin">It doesn't take a genius to see why<br></br> switching to the Galaxy S9 | S9+ is a good idea.
                     </p>
-                    <div>
-                        <button className=" btn w-[300px] mt-5 bg-black text-white ">Learn more</button>
+                    <div className=" flex justify-center md:flex md:justify-start">
+                        <button className=" btn w-[150px] md:w-[300px] mt-5 bg-black text-white ">Learn more</button>
                     </div>
                 </div>
-                <div>
-                    <img className=" h-[400px] " src="https://img.freepik.com/free-vector/internet-day-concept-illustration_114360-5303.jpg?t=st=1741081704~exp=1741085304~hmac=0806f05da9d6ef7c0af910a0c0eb773123f189958c8b7170457c621b693531db&w=740" alt="" />
+                <div className=" flex justify-start ">
+                    <img className="h-[200px] md:h-[400px]" src="https://img.freepik.com/free-vector/internet-day-concept-illustration_114360-5303.jpg?t=st=1741081704~exp=1741085304~hmac=0806f05da9d6ef7c0af910a0c0eb773123f189958c8b7170457c621b693531db&w=740" alt="" />
                 </div>
             </div>
         </div>
