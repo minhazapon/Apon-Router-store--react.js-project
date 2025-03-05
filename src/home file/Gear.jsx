0cfe2b2bb651969533fbbbdbe4169bdd457a1e6e
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 21:24:07 -0800

    added file

--- src/home file/Gear.jsx
+++ src/home file/Gear.jsx
@@ -8,6 +8,10 @@ function Gear() {
                 <div className=" mt-20 ml-10 mr-10 p-5 ">
                     <p className=" text-center text-xl md:text-5xl font-bold text-black ">Gear Router</p>
                     <p className=" text-center mt-6 text-xs md:text-sm  font-mono text-gray-400 ">The Gear IconX are cutting-edge wireless earbuds—as well as a portable music library,4 fitness tracker, and personal coach.5<br></br> Whether standalone or connected to your Note9,6 you can listen to your playlists and podcasts, maximize your workouts, and log your progress, all without touching your router.</p>
+
+                    <div>
+                        <img className=" rounded-xl mt-6 " src="https://static1.howtogeekimages.com/wordpress/wp-content/uploads/2021/05/52624435522_5ef2c34484_o.jpg" alt="" />
+                    </div>
                 </div>
             </div>
         </>
