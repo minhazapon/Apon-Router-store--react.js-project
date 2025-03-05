269a9ede5e9a3490bfef1444a2fd4e338f359fa8
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 20:59:45 -0800

    added file

--- src/home file/Explore.jsx
+++ src/home file/Explore.jsx
@@ -5,14 +5,14 @@ function Explore() {
         <div className=" mt-20 ml-10 mr-10 p-5 ">
             <div>
                 <div>
-                    <p className=" text-center text-3xl">Explore other best <span className=" text-4xl text-[#72afa6]">ROUTER</span> devices.</p>
+                    <p className=" text-center text-xl  md:text-3xl">Explore other best <span className=" text-xs md:text-4xl text-[#72afa6]">ROUTER</span> devices.</p>
                 </div>
                 <div>
-                    <div className=" grid  md:grid-cols-4 gap-5 mt-10 ">
-                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
-                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-vector/wifi-router-realistic-wireless-broadband-modem-with-antennas-3d-mockup-ethernet-router-network-connection-internet-access-isolated-white-background-vector-illustration_341509-3324.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
-                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/3d-rendering-wireless-router-white-background_493806-5172.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
-                        <img className=" h-[200px] w-[300px] " src="https://img.freepik.com/premium-photo/modern-modem-with-blue-light_96743-255.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                    <div className=" grid  grid-cols-4 md:grid-cols-4 gap-5 mt-10 ">
+                        <img className=" h-[100px] w-[100px]  md:h-[200px]  md:w-[300px] " src="https://img.freepik.com/premium-photo/wireless-network-router-white-background_1126594-2605.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[100px] w-[100px]  md:h-[200px]  md:w-[300px] " src="https://img.freepik.com/premium-vector/wifi-router-realistic-wireless-broadband-modem-with-antennas-3d-mockup-ethernet-router-network-connection-internet-access-isolated-white-background-vector-illustration_341509-3324.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[100px] w-[100px]  md:h-[200px]  md:w-[300px] " src="https://img.freepik.com/premium-photo/3d-rendering-wireless-router-white-background_493806-5172.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
+                        <img className=" h-[100px] w-[100px]  md:h-[200px]  md:w-[300px] " src="https://img.freepik.com/premium-photo/modern-modem-with-blue-light_96743-255.jpg?ga=GA1.1.333221720.1700564015&semt=ais_hybrid" alt="" />
                     </div>
                 </div>
             </div>
