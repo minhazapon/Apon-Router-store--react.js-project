fa3f026095c59e7af991a8d343d0fd7e7eb7e4a9
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 21:06:28 -0800

    added file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import Connect from "./home file/Connect"
 import Explore from "./home file/Explore"
 import HomeBanner from "./home file/HomeBanner"
+import See from "./home file/See"
 import Super from "./home file/Super"
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
             <Explore></Explore>
             <Super></Super>
             <Connect></Connect>
+            <See></See>
         </div>
     )
 }
--- src/home file/See.jsx
+++ src/home file/See.jsx
@@ -1,10 +1,28 @@
 
 
 function See() {
-    return (
-        <div>
 
-        </div>
+    return (
+        <>
+            <div className=" bg-no-repeat bg-cover p-6 "
+                style={{
+                    backgroundImage: "url(https://img.freepik.com/free-photo/vivid-blurred-colorful-wallpaper-background_58702-3772.jpg?t=st=1741083102~exp=1741086702~hmac=ad5a663fcb62b21361e66a6f73b86c17a6091342ee996d13215e3b836dab0f31&w=1060)",
+                }}
+            >
+                <div className=" mt-20 ml-10 mr-10 flex-col md:flex-row lg:flex-row flex justify-between items-center ">
+                    <div>
+                        <p className=" text-5xl font-bold ">The new super<br></br> powerful Galaxy<br></br> Note9</p>
+                        <p className=" mt-5 text-xl ">Meet the latest member of the router family.</p>
+                        <div>
+                            <button className="btn bg-black text-white w-[300px] mt-8">Buy Now</button>
+                        </div>
+                    </div>
+                    <div className="mt-6 md:mt-0">
+                        <img className=" h-[300px] rounded-xl " src="https://img.freepik.com/free-photo/high-angle-wi-fi-router-with-vacuum-cleaner_23-2148779238.jpg?t=st=1741082917~exp=1741086517~hmac=81d6fb2c739f2f3468a3c6464260437fcc9f417ebb164a0016f4673cf70c8614&w=1060" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
