b9df36bbb2c4079d756ff5ecc2701ee770d30c8c
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-04 22:12:48 -0800

    added file

--- src/about file/About.jsx
+++ src/about file/About.jsx
@@ -1,10 +1,12 @@
 import AboutBanner from "./AboutBanner"
+import Repairing from "./Repairing"
 
 
 function About() {
     return (
         <>
             <AboutBanner></AboutBanner>
+            <Repairing></Repairing>
         </>
     )
 }
