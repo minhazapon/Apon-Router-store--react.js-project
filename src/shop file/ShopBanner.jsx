5a93e2a90337e71646f86a2082da37ed80ae4b69
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-05 21:08:45 -0800

    added file

--- src/shop file/ShopBanner.jsx
+++ src/shop file/ShopBanner.jsx
@@ -6,17 +6,12 @@ function ShopBanner() {
             <div
                 className="hero h-[500px]"
                 style={{
-                    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                    backgroundImage: "url(https://img.freepik.com/free-vector/shopping-online-website-futuristic-design_23-2148528312.jpg?t=st=1741237698~exp=1741241298~hmac=a2d6752ad16e0d56d500c305aba49222756f9e0da1e28ee153c0dcb579d7cf95&w=1060)",
                 }}>
                 <div className="hero-overlay"></div>
                 <div className="hero-content text-neutral-content text-center">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-5xl font-bold text-white">Shopping With Us</h1>
                     </div>
                 </div>
             </div>
