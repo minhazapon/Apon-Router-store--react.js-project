2360aec1d22c1a78e9eb1b77a46d1796a605d767
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-03-05 20:27:54 -0800

    added file

--- src/shop file/Shop.jsx
+++ src/shop file/Shop.jsx
@@ -1,10 +1,21 @@
-
+import { useState } from "react"
+import Cart from "./Cart"
+import Items from "./Items"
 
 function Shop() {
 
+    const [cart, setCart] = useState([])
+
+    const addCart = (allProducts) => {
+
+        setItem([...Cart, allProducts])
+
+    }
+
     return (
         <div>
-
+            <Items></Items>
+            <Cart></Cart>
         </div>
     )
 }
